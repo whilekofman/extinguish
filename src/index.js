1626836b0fb3e2e59a16e89df558218dcf69ce36
Eugene Kofman <eugenekofman@gmail.com>
2022-09-16 13:38:23 -0400

    start for the day

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const main = document.getElementById('.gameboard')
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    const heading = new Heading('Hey Programmers!!');
+    console.log(heading.heading);
+    root.innerHTML = heading.heading;
+})
