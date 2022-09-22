2e146599b00a4c7ea4a6fe189782b824bdd666ca
Eugene Kofman <eugenekofman@gmail.com>
2022-09-22 13:29:56 -0400

    instructions and header work

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const View = require("../src/scripts/extinguish-view.js")
 const Game = require("../src/scripts/game.js")
 const Board = require("../src/scripts/board.js")
-const Splash = require("../src/scripts/splash.js")
+
 window.View = View;
 document.addEventListener('DOMContentLoaded', () => {
     const main = document.getElementById('gameboard')
     new View(main, Game);
-    const splashScreen = document.getElementById("splash") 
-    // new View(main, Board);
-    new Splash(splashScreen)
+
     
 })
