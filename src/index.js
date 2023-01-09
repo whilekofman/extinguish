a9ca52ceca427fa9926c226b6c4079ae91120e9d
Eugene Kofman <eugenekofman@gmail.com>
2023-01-09 13:36:36 -0500

    fix live page issue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const View = require("../src/scripts/extinguish-view.js")
 const Game = require("../src/scripts/game.js")
 const Board = require("../src/scripts/board.js")
+const About = require ("../src/scripts/about")
 
 window.View = View;
+window.View = About;
 document.addEventListener('DOMContentLoaded', () => {
     const main = document.getElementById('gameboard')
     const instructions = document.getElementById('about')
---
+++ src/scripts/about.js
@@ -0,0 +1,4 @@
+class About {
+
+} 
+module.exports = About
